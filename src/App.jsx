01b7053d3324bd6dfa24217f594af0f263eb7efb
Krutagna Patel <kpatel.kp7016@gmail.com>
2023-04-10 22:29:03 +0530

    added some components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Layout/Header/Header.jsx";
 import Meals from "./components/Meals/Meals.jsx";
+import Cart from "./components/Cart/Cart.jsx";
 import React from "react";
 
 function App() {
   return (
     <React.Fragment>
+      <Cart />
       <Header />
       <main>
         <Meals />
--- src/components/Meals/AvailableMeals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals/AvailableMeals.jsx
@@ -1,14 +1,23 @@
+import Card from "../../UI/Card/Card.jsx";
+import MealItem from "../MealItem/MealItem.jsx";
 import DUMMY_MEALS from "./data.js";
 import classes from "./AvailableMeals.module.css";
 
 function AvailableMeals() {
   return (
     <section className={classes.meals}>
-      <ul>
-        {DUMMY_MEALS.map((meal, index) => (
-          <li key={meal.id}>{meal.name}</li>
-        ))}
-      </ul>
+      <Card>
+        <ul>
+          {DUMMY_MEALS.map((meal) => (
+            <MealItem
+              key={meal.id}
+              name={meal.name}
+              description={meal.description}
+              price={meal.price}
+            />
+          ))}
+        </ul>
+      </Card>
     </section>
   );
 }
